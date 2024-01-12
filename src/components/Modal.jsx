2906270293070f54347705621419836437f2ffb1
Kateryna Belchynska <72660010+kbelchynska@users.noreply.github.com>
2024-01-12 14:40:13 +0200

    Fix modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,11 +1,19 @@
 import { forwardRef, useImperativeHandle, useRef } from "react";
 import { createPortal } from "react-dom";
 
-function Modal({ open, children }) {
+function Modal({ open, children, onClose }) {
   const dialog = useRef();
 
+  useEffect(() => {
+    if (open) {
+      dialog.current.showModal();
+    } else {
+      dialog.current.close();
+    }
+  }, [open]);
+
   return createPortal(
-    <dialog className="modal" ref={dialog} open={open}>
+    <dialog className="modal" ref={dialog} open={open} onClose={onClose}>
       {children}
     </dialog>,
     document.getElementById("modal"),
